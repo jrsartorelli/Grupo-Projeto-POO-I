8ea471a58f6ac63e9b192b1f81fbcc1d5a1c8c50
José Ricardo Sartorelli <jrsartorelli@gmail.com>
2022-12-06 22:45:15 -0300

    Atualização classe Pessoa

--- src/Pessoa.java
+++ src/Pessoa.java
@@ -8,4 +8,28 @@ public class Pessoa {
         this.idade = idade;
         this.altura = altura;
     }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public float getAltura() {
+        return altura;
+    }
+
+    public void setAltura(float altura) {
+        this.altura = altura;
+    }
 }
