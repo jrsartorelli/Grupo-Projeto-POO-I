db0a175ebd28e31aa7a1a437cc5dfea8bf66afea
José Ricardo Sartorelli <jrsartorelli@gmail.com>
2022-12-07 17:25:21 -0300

    Atualização

---
+++ src/Calculadora.java
@@ -0,0 +1,49 @@
+public class Calculadora {
+    private Double valor1;
+    private Double valor2;
+
+    public Calculadora(double valor1, double valor2) {
+        this.valor1 = valor1;
+        this.valor2 = valor2;
+    }
+
+    private boolean isValoresDefinidos(){
+        if(valor1 != null && valor2 != null){
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public Double somar(){
+        if (isValoresDefinidos()){
+            return valor1 + valor2;
+        } else {
+            return null;
+        }
+    }
+
+    public Double subtrair(){
+        if (isValoresDefinidos()){
+            return valor1 - valor2;
+        } else {
+            return null;
+        }
+    }
+
+    public Double multiplicar(){
+        if (isValoresDefinidos()){
+            return valor1 * valor2;
+        } else {
+            return null;
+        }
+    }
+
+    public Double dividir(){
+        if (isValoresDefinidos()){
+            return valor1 / valor2;
+        } else {
+            return null;
+        }
+    }
+}
