d31d516c3442a3582bc0362f0ac00fe5c9a5f060
José Ricardo Sartorelli <jrsartorelli@gmail.com>
2022-12-07 16:31:39 -0300

    Atualização

--- src/NumeroParImpar.java
+++
@@ -1,9 +0,0 @@
-public class NumeroParImpar {
-    public static boolean isPar(int numero){
-        if (numero % 2 == 0){
-            return true;
-        } else {
-            return false;
-        }
-    }
-}
