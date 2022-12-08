43c48ce9bd72fd69c2e35bef4c9d23a498ac5b94
José Ricardo Sartorelli <jrsartorelli@gmail.com>
2022-12-08 14:59:51 -0300

    Código funcional

--- src/ada/exercise/morse/Main.java
+++ src/ada/exercise/morse/Main.java
@@ -1,9 +1,29 @@
 package ada.exercise.morse;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        for (int i = 0; i < AlfabetoPortugues.CARACTERES.length ; i++){
-            System.out.println(AlfabetoPortugues.CARACTERES[i] + " = " + CodigoMorse.CODIGOSMORSE[i]);
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Digite a mensagem em Português: ");
+        String frase = sc.nextLine().toLowerCase();
+        sc.close();
+
+        String[] palavras = frase.split(" ");
+        String saidaMorse = "";
+        for(int i = 0; i < palavras.length; i++){
+            System.out.println(palavras[i]);
+            char[] letras = palavras[i].toCharArray();
+            for (int j = 0; j < letras.length; j++){
+                for (int y = 0; y < AlfabetoPortugues.CARACTERES.length; y++){
+                    if (letras[j] == AlfabetoPortugues.CARACTERES[y]){
+                        saidaMorse += CodigoMorse.CODIGOSMORSE[y];
+                    }
+                }
+                saidaMorse += " ";
+            }
+            saidaMorse += "/ ";
         }
+        System.out.println(saidaMorse);
     }
 }
