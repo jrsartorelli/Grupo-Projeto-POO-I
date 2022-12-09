650feed23e1fd9a6647bfcfc8eb756ac1f1bbb4d
mayh0x <marayah.love2@gmail.com>
2022-12-09 20:29:57 -0300

    Update Exercise

---
+++ src/ada/projeto_final/lutadores/Jogador.java
@@ -0,0 +1,70 @@
+package ada.projeto_final.lutadores;
+
+// import java.util.ArrayList;
+// import java.util.List;
+
+public class Jogador {
+    private String nome;
+    private Integer vida = 1000;
+    private Integer nivel = 1;
+    // private List<Pokemon> listaPokemons = new ArrayList<>();
+    private Pokemon pokemon;
+    private Integer numRevives = 1;
+
+//    public Jogador(String nome) {
+//        this.nome = nome;
+//    }
+
+    public Jogador(String nome, Pokemon pokemon) {
+        this.nome = nome;
+        this.pokemon = pokemon;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public Integer getVida() {
+        return vida;
+    }
+
+    public Integer getNivel() {
+        return nivel;
+    }
+
+//    public List<Pokemon> getListaPokemons() {
+//        return listaPokemons;
+//    }
+
+    public Pokemon getPokemon() {
+        return pokemon;
+    }
+
+    public Integer getNumRevives() {
+        return numRevives;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setVida(Integer vida) {
+        this.vida = vida;
+    }
+
+    public void setNivel(Integer nivel) {
+        this.nivel = nivel;
+    }
+
+//    public void setListaPokemons(List<Pokemon> listaPokemons) {
+//        this.listaPokemons = listaPokemons;
+//    }
+
+    public void setPokemon(Pokemon pokemon) {
+        this.pokemon = pokemon;
+    }
+
+    public void setNumRevives(Integer numRevives) {
+        this.numRevives = numRevives;
+    }
+}
--- src/ada/projeto_final/lutadores/Pokemon.java
+++ src/ada/projeto_final/lutadores/Pokemon.java
@@ -1,4 +1,8 @@
 package ada.projeto_final.lutadores;
 
+import java.util.Map;
+
 public abstract class Pokemon {
+    private String nome;
+    private float vida;
 }
