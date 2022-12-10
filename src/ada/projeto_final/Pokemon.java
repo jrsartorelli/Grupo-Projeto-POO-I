aa85adbd31a1a14f4e1134045d227f36ffb557af
José Ricardo Sartorelli <jrsartorelli@gmail.com>
2022-12-10 13:20:59 -0300

    Atualizando a classe Pokemon

--- src/ada/projeto_final/Pokemon.java
+++ src/ada/projeto_final/Pokemon.java
@@ -1,6 +1,32 @@
 package ada.projeto_final;
 
 public class Pokemon {
-    private String nome;
+    private final String nome;
     private float vida;
+    private final String[] ataques;
+
+    // Cria um novo pokémon com o nome e os ataques especificados
+    public Pokemon(String nome, String[] ataques) {
+        this.nome = nome;
+        this.ataques = ataques;
+        this.vida = 1000; // Valor padrão de vida para todos os pokémons
+    }
+
+    // Retorna se o pokémon ainda está vivo (se a vida ainda é maior que 0)
+    public boolean estaVivo() {
+        return vida > 0;
+    }
+
+    // Aplica o dano especificado ao pokémon
+    public void applyDamage(int damage) {
+        hp = Math.max(0, hp - damage);
+    }
+
+    // Exibe os ataques do pokémon
+    public void mostrarAtaques() {
+        for (int i = 0; i < ataques.length; i++) {
+            System.out.println((i + 1) + " - " + ataques[i]);
+        }
+    }
 }
+
