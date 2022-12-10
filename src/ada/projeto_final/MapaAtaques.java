4a5f9795990df2079510594feabfa42c9d9a8d6a
José Ricardo Sartorelli <jrsartorelli@gmail.com>
2022-12-10 17:29:50 -0300

    Criando classe MapaAtaques, esta classe vai servir de consulta para saber os respectivos ataques de cada tipo de Pokémon

---
+++ src/ada/projeto_final/MapaAtaques.java
@@ -0,0 +1,11 @@
+package ada.projeto_final;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MapaAtaques {
+    private static Map<String, String[]> mapaAtaques = new HashMap<>() {{
+        put("Charmander", new String[]{"FireBall", "FlameThrower", "FireBlast"});
+
+    }};
+}
