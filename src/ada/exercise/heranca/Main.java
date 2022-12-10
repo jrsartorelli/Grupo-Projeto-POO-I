7de8ed5a9848ebaa47a8d5162d3c2c8a5ae1daa2
José Ricardo Sartorelli <jrsartorelli@gmail.com>
2022-12-10 12:35:06 -0300

    Refatorando a Main

--- src/ada/exercise/heranca/Main.java
+++ src/ada/exercise/heranca/Main.java
@@ -6,16 +6,14 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Digite a opção com a forma geométrica que você quer calcular:\n" +
+        int tipoForma;
+        tipoForma = lerIntUsuario("Digite a opção com a forma geométrica que você quer calcular:\n" +
                          "1 - Círculo\n2 - Triângulo\n3 - Retângulo\n4 - Quadrado\n" +
                          "Escolha uma opção (1, 2, 3 ou 4): ");
-        int forma = sc.nextInt();
-        sc.nextLine();
 
-        switch (forma) {
+        switch (tipoForma) {
             case 1:
-                criarCirculo(sc);
+                criarCirculo();
                 break;
             case 2:
 
@@ -28,30 +26,41 @@ public class Main {
                 System.err.println("Erro: opção escolhida inválida.");
                 break;
         }
-        sc.close();
     }
 
-    public static void criarCirculo(Scanner sc) {
-        double raio = leDoubleUsuario(sc, "Digite o raio do círculo: ");
-        String cor = leStringUsuario(sc, "Digite a cor do círculo: ");
+    public static void criarCirculo() {
+        double raio = lerDoubleUsuario("Digite o raio do círculo: ");
+        String cor = lerStringUsuario("Digite a cor do círculo: ");
         Circulo circulo = new Circulo(raio, cor);
         System.out.println(circulo);
         System.out.println("Área: " + circulo.area());
         System.out.println("Diâmetro: " + circulo.getDiametro());
     }
 
-    private static String leStringUsuario(Scanner input, String mensagem) {
+    private static String lerStringUsuario(String mensagem) {
+        Scanner input = new Scanner(System.in);
         String valorRecebido;
         System.out.print(mensagem);
         valorRecebido = input.nextLine();
+        input.close();
         return valorRecebido;
     }
 
-    private static double leDoubleUsuario(Scanner input, String mensagem) {
+    private static int lerIntUsuario(String mensagem) {
+        Scanner input = new Scanner(System.in);
+        int valorRecebido;
+        System.out.print(mensagem);
+        valorRecebido = input.nextInt();
+        input.close();
+        return valorRecebido;
+    }
+
+    private static double lerDoubleUsuario(String mensagem) {
+        Scanner input = new Scanner(System.in);
         double valorRecebido;
         System.out.print(mensagem);
         valorRecebido = input.nextDouble();
-        input.nextLine();
+        input.close();
         return valorRecebido;
     }
 }
