6e7c00633549061beb9ae3a5b7226ed5b0d71a3b
José Ricardo Sartorelli <jrsartorelli@gmail.com>
2022-12-10 19:45:28 -0300

    Gerando valores aleatáorios para os ataques

--- src/ada/projeto_final/MapaAtaques.java
+++ src/ada/projeto_final/MapaAtaques.java
@@ -5,15 +5,20 @@ import java.util.Map;
 import java.util.Random;
 
 public class MapaAtaques {
+    //Mapa associando o Pokémon específico aos seu possíveis ataques
     private static Map<String, String[]> mapaAtaques = new HashMap<>() {{
         put("Charmander", new String[]{"Fire Ball", "Flame Thrower", "Fire Blast"});
         put("Bulbasaur", new String[]{"Take Down", "Petal Dance", "Solar Beam"});
         put("Squirtle", new String[]{"Rapid Spin", "Water Gun", "Aqua Tail"});
     }};
+
+    //Mapa para o valor para cada tipo de ataque
     private static Map<String, Integer> mapaValorAtaque = new HashMap<>();
+
     private static final Integer VALOR_ATAQUE_BASE = 300;
     private static Random random = new Random();
 
+    //Insere de forma randômica os valores para todos os ataques listados
     public static void inicializarValoresAtaque(){
         for (String[] ataques : mapaAtaques.values()) {
             for (String ataque: ataques) {
