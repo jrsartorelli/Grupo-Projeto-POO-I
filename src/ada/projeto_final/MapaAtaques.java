dbe3390ab86c21d92f95525cf4d5bfb037967d1d
José Ricardo Sartorelli <jrsartorelli@gmail.com>
2022-12-11 15:11:20 -0300

    Divisão de Niveis para os Pokemons

--- src/ada/projeto_final/MapaAtaques.java
+++ src/ada/projeto_final/MapaAtaques.java
@@ -11,41 +11,55 @@ public class MapaAtaques {
     private static final Integer VALOR_ATAQUE_BASE_NIVEL1 = 300;
     private static final Integer VALOR_ATAQUE_BASE_NIVEL2 = 400;
     private static final Integer VALOR_ATAQUE_BASE_NIVEL3 = 500;
-    private static Random random = new Random();
+    private static final Random random = new Random();
 
-    private static Map<String, String[]> mapaAtaquesNivel1 = new HashMap<>() {{
+    private static final Map<String, String[]> mapaAtaquesNivel1 = new HashMap<>() {{
         put("Charmander", new String[]{"Fire Ball", "Flame Thrower", "Fire Blast"});
         put("Bulbasaur", new String[]{"Take Down", "Petal Dance", "Solar Beam"});
         put("Squirtle", new String[]{"Bubble Beam", "Water Gun", "Aqua Tail"});
     }};
 
-    private static Map<String, String[]> mapaAtaquesNivel2 = new HashMap<>() {{
+    private static final Map<String, String[]> mapaAtaquesNivel2 = new HashMap<>() {{
         put("Charmeleon", new String[]{"Fire Spin", "Dragon Claw", "Headbutt"});
         put("Ivysaur", new String[]{"Vine Whip", "Razor Leaf", "Tackle"});
         put("Wartortle", new String[]{"Hydro Pump", "Rapid Spin", "Bite"});
     }};
 
-    private static Map<String, String[]> mapaAtaquesNivel3 = new HashMap<>() {{
+    private static final Map<String, String[]> mapaAtaquesNivel3 = new HashMap<>() {{
         put("Charizard", new String[]{"Slash", "Steel Wing", "Seismic Toss"});
         put("Venusaur", new String[]{"Sweet Scent", "Sleep Powder", "Frenzy Plant"});
         put("Blastoise", new String[]{"Withdraw", "Rain Dance", "Skull Bash"});
     }};
 
-    //Mapa associando o Pokémon específico aos seu possíveis ataques
-    private static Map<Integer, Map> mapaPokemons = new HashMap<>() {{
+    //Mapa associando o Nível do Pokémon aos seus respetivos ataques
+    private static final Map<Integer, Map> mapaPokemons = new HashMap<>() {{
         put(NIVEL_1, mapaAtaquesNivel1);
         put(NIVEL_2, mapaAtaquesNivel2);
         put(NIVEL_3, mapaAtaquesNivel3);
     }};
 
-    //Mapa para o valor para cada tipo de ataque
-    private static Map<String, Integer> mapaValorAtaque = new HashMap<>();
+    //Mapa com o valor para cada tipo de ataque
+    private static final Map<String, Integer> mapaValorAtaque = new HashMap<>();
 
     //Insere de forma randômica os valores para todos os ataques listados
     public static void inicializarValoresAtaque(){
+        int valorAtaqueGerado;
         for (String[] ataques : mapaAtaquesNivel1.values()) {
-            for (String ataque: ataques) {
-                mapaValorAtaque.put(ataque, (VALOR_ATAQUE_BASE_NIVEL1 + random.nextInt(0, 300)));
+            for (String ataque : ataques) {
+                valorAtaqueGerado = (VALOR_ATAQUE_BASE_NIVEL1 + random.nextInt(0, 300));
+                mapaValorAtaque.put(ataque, valorAtaqueGerado);
+            }
+        }
+        for (String[] ataques : mapaAtaquesNivel2.values()) {
+            for (String ataque : ataques) {
+                valorAtaqueGerado = (VALOR_ATAQUE_BASE_NIVEL2 + random.nextInt(0, 300));
+                mapaValorAtaque.put(ataque, valorAtaqueGerado);
+            }
+        }
+        for (String[] ataques : mapaAtaquesNivel3.values()) {
+            for (String ataque : ataques) {
+                valorAtaqueGerado = (VALOR_ATAQUE_BASE_NIVEL3 + random.nextInt(0, 300));
+                mapaValorAtaque.put(ataque, valorAtaqueGerado);
             }
         }
     }
