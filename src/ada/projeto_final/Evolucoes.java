736c601480ddd196637e6e27c84f3c8eb329dd97
José Ricardo Sartorelli <jrsartorelli@gmail.com>
2022-12-11 16:18:28 -0300

    Inclusão de novos Pokémons

--- src/ada/projeto_final/Evolucoes.java
+++ src/ada/projeto_final/Evolucoes.java
@@ -8,10 +8,66 @@ public class Evolucoes {
         {
             put("Charmander","Charmeleon");
             put("Charmeleon","Charizard");
+
             put("Bulbasaur","Ivysaur");
             put("Ivysaur","Venusaur");
+
             put("Squirtle","Wartortle");
             put("Wartortle","Blastoise");
+
+            put("Caterpie", "Metapod");
+            put("Metapod", "Butterfree");
+
+            put("Weedle", "Kakuna");
+            put("Kakuna", "Beedrill");
+
+            put("Pidgey", "Pidgeotto");
+            put("Pidgeotto", "Pidgeot");
+
+            put("Nidoran", "Nidorina");
+            put("Nidorina", "Moon Stone");
+
+            put("Vulpix", "Fire Stone");
+            put("Fire Stone", "Ninetales");
+
+            put("Oddish", "Gloom");
+            put("Gloom", "Leaf Stone");
+
+            put("Poliwag", "Poliwhirl");
+            put("Poliwhirl", "Poliwrath");
+
+            put("Abra", "Kadabra");
+            put("Kadabra", "Alakazam");
+
+            put("Machop", "Machoke");
+            put("Machoke", "Machamp");
+
+            put("Bellsprout", "Weepinbell");
+            put("Weepinbell", "Victreebel");
+
+            put("Geodude", "Graveler");
+            put("Graveler", "Golem");
+
+            put("Gastly", "Haunter");
+            put("Haunter", "Gengar");
+
+            put("Eevee", "Water Stone");
+            put("Water Stone", "Vaporeon");
+
+            put("Dratini", "Dragonair");
+            put("Dragonair", "Dragonite");
+
+            put("Ricardo", "Super Ricardo");
+            put("Super Ricardo", "Ricardão");
+
+            put("Marayah", "Super Marayah");
+            put("Super Marayah", "Fera dos Kahoot´s");
+
+            put("Gabriel", "Super Gabriel");
+            put("Super Gabriel", "Mega Power Gabriel");
+
+            put("Leomim", "Super Leomim");
+            put("Super Leomim", "Consultor Pokémon");
         }
     };
 
