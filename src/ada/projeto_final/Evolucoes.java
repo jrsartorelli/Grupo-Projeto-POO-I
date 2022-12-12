41fabfcde4b80c653f514b20c84a3b63e72425f8
José Ricardo Sartorelli <jrsartorelli@gmail.com>
2022-12-12 12:57:18 -0300

    Refatorando

--- src/ada/projeto_final/Evolucoes.java
+++ src/ada/projeto_final/Evolucoes.java
@@ -3,7 +3,7 @@ package ada.projeto_final;
 import java.util.HashMap;
 
 public class Evolucoes {
-    private HashMap<String, String> evolucoes =new HashMap<>()
+    private final HashMap<String, String> evolucoes =new HashMap<>()
     {
         {
             put("Charmander","Charmeleon");
